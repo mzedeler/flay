98dd32a618ea33bd6fa1e67b1cc8c592d483fc70
Michael Zedeler <michael@zedeler.dk>
2023-01-10 13:29:20 +0100

    Cleanup + ignore node_modules.

--- src/utils/traverse.ts
+++ src/utils/traverse.ts
@@ -6,11 +6,14 @@ import type { PathEntry } from '../types/PathEntry'
 export async function* traverse(dir: string): AsyncGenerator<PathEntry> {
   const dh = await opendir(dir)
   for await (const dirent of dh) {
+    if (dirent.isDirectory() && dirent.name === 'node_modules') {
+      continue
+    }
     yield {
       path: join(dir, dirent.name),
       dirent
     }
-    if (dirent.isDirectory() && dirent.name !== 'node_modules') {
+    if (dirent.isDirectory()) {
       for await (const pathEntry of await traverse(join(dir, dirent.name))) {
         yield pathEntry
       }
