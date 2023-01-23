e6f2628d2ffdb2f8bc48d69dde58403d3bb6f2bb
michael@zedeler.dk <michael@zedeler.dk>
2023-01-23 19:03:02 +0100

    Refactor to extract runner.

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,7 @@
 import chalk from 'chalk'
-import { dirname } from 'node:path'
+import { validate } from './validate'
 
-import type { PathEntry } from './types/PathEntry'
-
-import { getPathEntryMap } from './utils/getPathEntryMap'
-import { getIndexFiles } from './utils/getIndexFiles'
-import { getCompoundDirectories } from './utils/getCompoundDirectories'
-import { getCollections } from './utils/getCollections'
-
-type Violation = {
-  pathEntries: PathEntry[]
-  title: string
-  message: string
-}
-
-type PathEntryMap = Record<string, PathEntry>
-
-async function* validate(dir: string): AsyncGenerator<Violation> {
-  const pathEntryMap = await getPathEntryMap(dir)
-  const indexFiles = await getIndexFiles(dir)
-  const compoundDirectories = await getCompoundDirectories(dir)
-  const collections = await getCollections(dir)
-
-  const indexFileDirectoryPaths = Object.keys(indexFiles).map(dirname)
-
-  const getCompoundDirectoriesWithoutIndexFiles = () => {
-    const result = compoundDirectories
-    for (const directory of indexFileDirectoryPaths) {
-      delete result[directory]
-    }
-    return Object.values(result)
-  }
-
-  yield {
-    title: 'Compound directories without index files',
-    message: 'These compound directories are missing index files.',
-    pathEntries: getCompoundDirectoriesWithoutIndexFiles()
-  }
-
-  const getCollectionsWithIndexFiles = () => {
-    const result: PathEntryMap = {}
-    for (const path of indexFileDirectoryPaths) {
-      if (collections[path]) {
-        result[path] = collections[path]
-      }
-    }
-    return Object.values(result)
-  }
-
-  yield {
-    title: 'Collections with index files',
-    message: 'These collections have unexpected index files.',
-    pathEntries: getCollectionsWithIndexFiles()
-  }
-}
-
-async function main() {
+(async function() {
   let failed
 
   const indent = (message: string) => message.replace(new RegExp('^', 'g'), '  ')
@@ -77,6 +23,4 @@ async function main() {
   if (failed) {
     process.exit(-1)
   }
-}
-
-main()
+})()
