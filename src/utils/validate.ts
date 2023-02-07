6cf0e845969561837f7fa49983bf287a7a8802f0
Michael Zedeler <michael@zedeler.dk>
2023-02-07 14:22:48 +0100

    Remove unneeded import.

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -3,13 +3,11 @@ import { dirname } from 'node:path'
 import { getCollections } from './getCollections'
 import { getCompoundDirectories } from './getCompoundDirectories'
 import { getIndexFiles } from './getIndexFiles'
-import { getPathEntryMap } from './getPathEntryMap'
 
 import type { Violation } from '../types/Violation'
 import type { PathEntryMap } from '../types/PathEntryMap'
 
 export async function* validate(dir: string): AsyncGenerator<Violation> {
-    const pathEntryMap = await getPathEntryMap(dir)
     const indexFiles = await getIndexFiles(dir)
     const compoundDirectories = await getCompoundDirectories(dir)
     const collections = await getCollections(dir)
